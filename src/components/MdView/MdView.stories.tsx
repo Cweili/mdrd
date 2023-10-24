2595cdf61ebd8954afbed777d9223c7ed9be53f1
Cweili <3weili@gmail.com>
2023-10-24 17:18:29 +0800

    docs: fix MdView docs title

--- src/components/MdView/MdView.stories.tsx
+++ src/components/MdView/MdView.stories.tsx
@@ -6,7 +6,7 @@ const sleep = (ms: number) => new Promise((resolve) => { setTimeout(resolve, ms)
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta: Meta<typeof MdView> = {
-  title: 'MdViewer',
+  title: 'MdView',
   component: MdView,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
